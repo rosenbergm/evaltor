5c3d2cb0ff44b726d33356c30646f5df59a75edb
Martin Rosenberg <nitram.rosenberg@gmail.com>
2026-03-20 21:30:27 +0100

    Removed unused code from UserAssignment

--- src/models/user_assignments.rs
+++ src/models/user_assignments.rs
@@ -1,45 +1,11 @@
-use sqlx::{QueryBuilder, Sqlite, SqlitePool};
+use sqlx::SqlitePool;
 use uuid::Uuid;
 
 use crate::{Points, models::assignment::Assignment};
 
-#[expect(dead_code)]
-pub struct UserAssignment {
-    pub id: Uuid,
-
-    pub user_id: Uuid,
-    pub assignment_id: Uuid,
-    pub class_id: Uuid,
-}
+pub struct UserAssignment;
 
 impl UserAssignment {
-    pub async fn assignments_for_user(
-        db: &SqlitePool,
-        user_id: Uuid,
-        class_id: Uuid,
-    ) -> sqlx::Result<Vec<Assignment>> {
-        let assignment_ids = sqlx::query!(
-            r#"SELECT
-                assignment_id as "assignment_id: uuid::Uuid"
-            FROM user_assignments WHERE user_id = ? AND class_id = ?"#,
-            user_id,
-            class_id
-        )
-        .fetch_all(db)
-        .await?
-        .into_iter()
-        .map(|record| record.assignment_id);
-
-        let mut builder: QueryBuilder<Sqlite> =
-            QueryBuilder::new("SELECT * FROM assignments WHERE id IN ");
-
-        builder.push_tuples(assignment_ids, |mut b, id| {
-            b.push_bind(id);
-        });
-
-        builder.build_query_as::<Assignment>().fetch_all(db).await
-    }
-
     pub async fn assignments_for_user_with_points(
         db: &SqlitePool,
         user_id: Uuid,
