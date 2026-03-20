de90c3599c544f3f5e2dfb05e74a849977b12aa0
Martin Rosenberg <nitram.rosenberg@gmail.com>
2026-03-20 21:29:41 +0100

    Add download attempt code button

--- src/routes/attempt.rs
+++ src/routes/attempt.rs
@@ -2,10 +2,11 @@ use askama::Template;
 use axum::{
     Router,
     extract::{Path, State},
-    response::Html,
+    response::{Html, IntoResponse},
     routing::get,
 };
 use reqwest::StatusCode;
+use tokio::fs;
 use uuid::Uuid;
 
 use crate::{
@@ -15,7 +16,9 @@ use crate::{
 };
 
 pub fn router() -> axum::Router<EvaltorState> {
-    Router::new().route("/attempts/{id}/runners", get(get_runners))
+    Router::new()
+        .route("/attempts/{id}/runners", get(get_runners))
+        .route("/attempts/{id}/source", get(get_attempt_source))
 }
 
 async fn get_runners(
@@ -76,4 +79,47 @@ async fn get_runners(
     .render()
     .map(Html)
     .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
+}
+
+pub async fn get_attempt_source(
+    auth: auth::AuthUser,
+    State(state): State<EvaltorState>,
+    Path(attempt_id): Path<Uuid>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let submissions_dir = state.config.submissions;
+    let user_id = auth.id;
+
+    let attempt_user_id = sqlx::query!(
+        r#"SELECT user_id as "user_id: Uuid" FROM attempts WHERE id = ?"#,
+        attempt_id
+    )
+    .fetch_one(&state.db_pool)
+    .await
+    .map_err(|_| StatusCode::NOT_FOUND)?;
+
+    if attempt_user_id.user_id != user_id {
+        return Err(StatusCode::FORBIDDEN);
+    }
+
+    let assignment = sqlx::query!(
+        r#"SELECT assignment_id as "id: Uuid" FROM attempts WHERE id = ?"#,
+        attempt_id
+    )
+    .fetch_one(&state.db_pool)
+    .await
+    .map_err(|_| StatusCode::NOT_FOUND)?;
+
+    let assignment_id = assignment.id;
+
+    let source_path = submissions_dir
+        .join(assignment_id.to_string())
+        .join(user_id.to_string())
+        .join(attempt_id.to_string())
+        .join("main.py");
+
+    let source = fs::read_to_string(source_path)
+        .await
+        .map_err(|_| StatusCode::NOT_FOUND)?;
+
+    Ok(source)
 }
